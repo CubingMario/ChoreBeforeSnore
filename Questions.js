190ccf6c265f4f5e6eea8ec27816250aa2b47354
Shin-02 <73204645+Shin-02@users.noreply.github.com>
2022-03-13 05:59:43 -0400

    Create Questions.js

---
+++ Questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Cleanliness is next to "
+    ,answer: "Godliness",
+    options: [
+    "Godliness",
+    "Godlessness",
+    "Dirty Things",
+    "Wild Animals",
+    ,],
+    }
+        ,{
+    numb: 2,
+    question: "Cleanliness is important for our good",
+    answer: "Health",
+    options: [
+    "Wealth",
+    "Health",
+    "Respect",
+    "Brain",
+    ]
+    },
+    {
+    numb: 3,
+    question: "We should keep our surroundings",
+    answer: "Clean",
+    options: [
+    "Clean",
+    "Dirty",
+    "A Mess",
+    "Filled With Trash",
+    ]
+},
+    {
+    numb: 4,
+    question: "We can keep our Earth clean by reducing",
+    answer: "Pollution",
+    options: [
+    "Trees",
+    "Pollution",
+    "Houses",
+    "People",
+    ]
+    },
+    {
+    numb: 5,
+    question: "Throw trash",
+    answer: "Trash Cans",
+    options: [
+    "On Streets",
+    "In Trash Cans",
+    "In Cars",
+    "At Someone"
+    ]
+    },
+];
